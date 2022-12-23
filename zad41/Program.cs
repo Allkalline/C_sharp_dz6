d7029af1c6037d28fdba36fec0dc412528b44780
VakhrushevDmitry <dimavah@inbox.ru>
2022-12-23 11:51:42 +0300

    вроде работает

---
+++ zad41/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.Write("Введите числа через пробел: ");
+string Numbers = Console.ReadLine()!;
+
+int[] array = Array.ConvertAll(Numbers.Split(), int.Parse);
+
+
+int Res (int [] arr)
+{
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+        count++;
+}
+return count;
+}
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine($"Чисел больше 0 введено: {Res(array)}");
+
+
+
---
+++ zad45/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования. */
+
+int[] GetArray(int size, int minValue, int maxValue) 
+{
+    int[] res = new int[size];
+    for(int i=0; i<size; i++)
+    {
+        res[i] = new Random().Next(minValue,maxValue+1);
+        // Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+
+void PrintArray(int[] arr)
+{
+     for(int i=0; i<arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+void CopyArray (int[] arr, int [] arr2)
+{   
+    for(int i=0; i<arr.Length; i++)
+    {arr2[i]=arr[i];
+    Console.Write($"{arr2[i]} ");
+    }
+}
+int size = new Random().Next(5,10);
+int[] array = GetArray(size,0,9);
+int [] array2 = new int [size];
+PrintArray(array);
+Console.WriteLine();
+CopyArray(array, array2);
+
+
